87bf7db9f37740bb157dfeca8c4e1fdd328693eb
PatRademacher <prad2@pdx.edu>
2020-11-14 21:28:50 -0800

    trying to fix homepage deployment issue

--- src/components/main.js
+++ src/components/main.js
@@ -5,16 +5,19 @@ import Contact from './contact';
 import Projects from './projects';
 import Resume from './resume';
 import { Switch, Route } from 'react-router-dom';
+import {BrowserRouter} from 'react-router-dom';
 
 
 const Main = () => (
-    <Switch>
-        <Route exact path="/" component ={LandingPage} />
-        <Route path="/aboutme" component={AboutMe} />
-        <Route path="/resume" component={Resume} />
-        <Route path="/projects" component={Projects} />
-        <Route path="/contact" component={Contact} />
-    </Switch>
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component ={LandingPage} />
+            <Route path="/aboutme" component={AboutMe} />
+            <Route path="/resume" component={Resume} />
+            <Route path="/projects" component={Projects} />
+            <Route path="/contact" component={Contact} />
+        </Switch>
+    </BrowserRouter>
 )
 
 export default Main;
